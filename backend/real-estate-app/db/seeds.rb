28f4c13520fefa8f8a0aa5c3245ee9129bbe2923
arthur_song <arth3rs0ng@gmail.com>
2019-11-14 10:14:44 -0800

    adding seed data

--- backend/real-estate-app/db/seeds.rb
+++ backend/real-estate-app/db/seeds.rb
@@ -15,4 +15,8 @@ zip_code_1 = Zipcode.create(digits: 85746);
 # t.integer :sqft
 # t.integer :year_built
 
-home_1 = Home.create(zipcode: zip_code_1, price: 175000, address: "3037 W Utah St, Tucson, AZ 85746", bedrooms: 3, bathrooms: 2, sqft: 1071, );
+home_1 = Home.create(zipcode: zip_code_1, price: 175000, address: "3037 W Utah St, Tucson, AZ 85746", bedrooms: 3, bathrooms: 2, sqft: 1071, year_built: 1977 );
+home_2 = Home.create(zipcode: zip_code_1, price: 198500, address: "3627 W Avenida Fria, Tucson, AZ 85746", bedrooms: 3, bathrooms: 3, sqft: 1516, year_built: 1993 );
+home_3 = Home.create(zipcode: zip_code_1, price: 216500, address: "2971 W Calle Leonido, Tucson, AZ 85746", bedrooms: 4, bathrooms: 2, sqft: 1873, year_built: 2001 );
+home_4 = Home.create(zipcode: zip_code_1, price: 180500, address: "5513 S Stockwell Rd, Tucson, AZ 85746", bedrooms: 3, bathrooms: 2, sqft: 1234, year_built: 1997 );
+home_5 = Home.create(zipcode: zip_code_1, price: 150000, address: "7911 S Caballo Rd, Tucson, AZ 85746", bedrooms: 3, bathrooms: 2, sqft: 1520, year_built: 1971 );
