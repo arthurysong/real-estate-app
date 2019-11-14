e0e0e76d6af082576016e095bf1af95763563615
arthur_song <arth3rs0ng@gmail.com>
2019-11-14 10:15:19 -0800

    school has name atribute now

--- backend/real-estate-app/db/migrate/20191114023459_create_schools.rb
+++ backend/real-estate-app/db/migrate/20191114023459_create_schools.rb
@@ -2,6 +2,7 @@ class CreateSchools < ActiveRecord::Migration[6.0]
   def change
     create_table :schools do |t|
       t.integer :zipcode_id
+      t.string :name
       t.integer :rating
 
       t.timestamps
