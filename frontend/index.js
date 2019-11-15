1fc469ecab6e03e10b1785ff07fd5690a1828c8a
arthur_song <arth3rs0ng@gmail.com>
2019-11-15 14:12:30 -0600

    fetch now adds data to doc elements

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
     // h1 = document.$('h1');
     // h1.innerHTML = 
+    h1 = document.querySelector('h1');
+    p = document.querySelector('p');
     fetch('http://127.0.0.1:3000/cities/1')
         .then(resp => resp.json())
-        .then(json => console.log(json));
+        .then(json => {
+            h1.innerHTML = json.name;
+            p.innerHTML = 
+                `GDP per capita: ${json.gdp_per_capita}<br>
+                job growth in past year: ${json.job_growth_percentage}<br>
+                population: ${json.population}<br>
+                median price of home: ${json.median_homeprice}`
+        });
 })
