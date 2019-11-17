2ac87e5622ebd329217a879b8e22f9de84a4c415
arthur_song <arth3rs0ng@gmail.com>
2019-11-16 20:59:06 -0600

    js now creates a city class, which I dont understand the need for

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,13 @@
+class City {
+    constructor (name, gdp_per_capita, job_growth_percentage, population, median_homeprice) {
+        this.name = name;
+        this.gdp_per_capita = gdp_per_capita;
+        this.job_growth_percentage = job_growth_percentage;
+        this.population = population;
+        this.median_homeprice = median_homeprice;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     // h1 = document.$('h1');
     // h1.innerHTML = 
@@ -7,12 +17,13 @@ document.addEventListener('DOMContentLoaded', function(){
         .then(resp => resp.json())
         .then(json => {
             console.log(json)
-            h1.innerHTML = json.name;
+            const city = new City(json.name, json.gdp_per_capita, json.job_growth_percentage, json.population, json.median_homeprice);
+            h1.innerHTML = city.name;
             p.innerHTML = 
-                `GDP per capita: ${json.gdp_per_capita}<br>
-                job growth in past year: ${json.job_growth_percentage}%<br>
-                population: ${json.population}<br>
-                median price of home: ${json.median_homeprice}`
+                `GDP per capita: ${city.gdp_per_capita}<br>
+                job growth in past year: ${city.job_growth_percentage}%<br>
+                population: ${city.population}<br>
+                median price of home: ${city.median_homeprice}`
             select = document.querySelector('select');
             for (const zipcode of json.zipcodes){
                 option = document.createElement('option');
