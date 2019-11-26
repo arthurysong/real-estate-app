fbb91171874ee92a8cca51328a72f260e3342986
arthur_song <arth3rs0ng@gmail.com>
2019-11-26 09:54:08 -0600

    now will create a home and return json of home when new home form is submitted

--- backend/real-estate-app/app/controllers/homes_controller.rb
+++ backend/real-estate-app/app/controllers/homes_controller.rb
@@ -1,5 +1,21 @@
 class HomesController < ApplicationController
     def create
         puts params
+        # Home.create()
+        zipcode = Zipcode.find(params[:zipcode_id])
+        month = params[:date_sold][0..1].to_i
+        day = params[:date_sold][3..4].to_i
+        year = params[:date_sold][6..9].to_i
+        d = Date.new(year, month, day)
+        home = Home.create(
+            zipcode: zipcode, 
+            date_sold: d, 
+            price: params[:price], 
+            address: params[:address], 
+            bedrooms: params[:bedrooms], 
+            bathrooms: params[:bathrooms], 
+            sqft: params[:sqft], 
+            year_built: params[:year_built] )
+        render json: home
     end
 end
