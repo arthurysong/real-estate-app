da8389892365bd1a885c6f598753b0ce3842b6fa
arthur_song <arth3rs0ng@gmail.com>
2019-11-30 11:24:55 -0700

    created city create controller action

--- backend/real-estate-app/app/controllers/cities_controller.rb
+++ backend/real-estate-app/app/controllers/cities_controller.rb
@@ -9,4 +9,17 @@ class CitiesController < ApplicationController
         render json: cities
     end
 
+    def create
+        city = City.create(
+            name: params[:name],
+            median_homeprice: params[:median_homeprice],
+            home_value_growth: params[:home_value_growth],
+            population: params[:population],
+            median_age: params[:median_age],
+            median_household_income: params[:median_household_income],
+            income_growth: params[:income_growth],
+            job_growth_percentage: params[:job_growth_percentage]
+        )
+        render json: city, include: [:zipcodes]
+    end
 end
