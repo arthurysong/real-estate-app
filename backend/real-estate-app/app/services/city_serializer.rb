20b8c4de478d0b763eb20c1d05f7f1c2c922eaf1
arthur_song <arth3rs0ng@gmail.com>
2019-12-01 08:50:47 -0700

    city serializer now added

---
+++ backend/real-estate-app/app/services/city_serializer.rb
@@ -0,0 +1,17 @@
+class CitySerializer
+    def initialize (city)
+        @city = city
+    end
+
+    def to_serialized_json
+        options = {
+            include: {
+                zipcodes: {
+                    only: [:digits]
+                }
+            }
+            except: [:updated_at, :created_at]
+        }
+        @city.to_json(options)
+    end
+end
